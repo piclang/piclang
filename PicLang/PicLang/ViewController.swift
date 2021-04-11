0e5ee7387554a2012dff29bc2b337d416d0d0cd8
jiliMiniArm <picturechat@163.com>
2021-04-11 20:52:14 +0800

    first add file

---
+++ PicLang/PicLang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicLang
+//
+//  Created by Ni Jili on 2021/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
